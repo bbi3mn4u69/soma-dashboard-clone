7810fad8245b8e964cc7821e5b97ce82adf93e03
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-06-15 20:43:03 +1000

    navbar

---
+++ src/app/_components/shared/NavBar.tsx
@@ -0,0 +1,59 @@
+"use client";
+import SomaLogo from "../../image/soma.png";
+import Image from "next/image";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
+import { useState } from "react";
+const NavBar = () => {
+  const [activeItem, setActiveItem] = useState<string>("Portfolio");
+
+  const handleItemClick = (item: string) => {
+    setActiveItem(item);
+  };
+  return (
+    <div>
+      <Navbar className="border border-b-1 border-gray-200">
+        <NavbarBrand>
+          <Image src={SomaLogo} alt="logo" width={50} height={18} />
+        </NavbarBrand>
+        <NavbarContent
+          className="hidden gap-x-5 text-sm font-semibold leading-6 sm:flex"
+          justify="end"
+        >
+          {[
+            "Portfolio",
+            "About Us",
+            "Team",
+            "News",
+            "Jobs",
+            "Fellowship",
+            "Sign In",
+          ].map((item) => (
+            <NavbarItem
+              key={item}
+              className={`p-1 ${activeItem === item ? "border-b-2 border-purple-600" : ""}`}
+            >
+              <Link
+                color="foreground"
+                href={"#"}
+                onPress={() => {
+                  handleItemClick(item);
+                }}
+              >
+                {item}
+              </Link>
+            </NavbarItem>
+          ))}
+        </NavbarContent>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
