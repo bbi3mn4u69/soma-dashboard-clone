7b5ac5310111f75a33be26d32fb1af56a4534dcc
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-06-18 11:19:33 +1000

    side panel

---
+++ src/app/_components/DashBoard/SidePanel.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { GrayLogo } from "../../image/gray-logo";
+import NavButton from "./NavButton";
+import { Home, Portfolio, Infor, Feed, Saved, Admin, Stock } from "../Icon";
+import { useState } from "react";
+const SidePanel = () => {
+  const [activeButton, setActiveButton] = useState<string>("Home");
+  return (
+    <>
+      <div className="min-h-screen bg-purple-900">
+        <div className="flex items-center justify-center py-4">
+          <GrayLogo />
+        </div>
+        <div className="mx-1 flex flex-col items-center justify-center">
+          <NavButton
+            title="Home"
+            icon={<Home active={activeButton === "Home"} />}
+            onClick={() => setActiveButton("Home")}
+            isActive={activeButton === "Home"}
+          />
+          <NavButton
+            title="Portfolio"
+            icon={<Portfolio active={activeButton === "Portfolio"} />}
+            onClick={() => setActiveButton("Portfolio")}
+            isActive={activeButton === "Portfolio"}
+          />
+          <NavButton
+            title="Infor"
+            icon={<Infor active={activeButton === "Infor"} />}
+            onClick={() => setActiveButton("Infor")}
+            isActive={activeButton === "Infor"}
+          />
+          <NavButton
+            title="Feed"
+            icon={<Feed active={activeButton === "Feed"} />}
+            onClick={() => setActiveButton("Feed")}
+            isActive={activeButton === "Feed"}
+          />
+          <NavButton
+            title="Saved"
+            icon={<Saved active={activeButton === "Saved"} />}
+            onClick={() => setActiveButton("Saved")}
+            isActive={activeButton === "Saved"}
+          />
+          <NavButton
+            title="Admin"
+            icon={<Admin active={activeButton === "Admin"} />}
+            onClick={() => setActiveButton("Admin")}
+            isActive={activeButton === "Admin"}
+          />
+          <NavButton
+            title="Stock"
+            icon={<Stock active={activeButton === "Stock"} />}
+            onClick={() => setActiveButton("Stock")}
+            isActive={activeButton === "Stock"}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SidePanel;
