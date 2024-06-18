0e1d656efcb35ccf952a4bb097156aa6efcb4e0e
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-06-18 11:19:46 +1000

    navigation button

---
+++ src/app/_components/DashBoard/NavButton.tsx
@@ -0,0 +1,24 @@
+const NavButton = ({
+  icon,
+  title,
+  onClick,
+  isActive
+}: {
+  icon: React.ReactNode;
+  title: string;
+  onClick: () => void;
+  isActive: boolean;
+}) => {
+  return (
+    <>
+      <div className={`m-1 w-full rounded-md py-2 px-3 cursor-pointer ${isActive ? 'bg-purple-700' : 'bg-purple-900'}`} onClick={onClick}>
+        <div className="flex flex-col items-center justify-center space-y-1">
+          <div>{icon}</div>
+          <div className={`text-xs font-medium  ${isActive ? 'text-white' : 'text-gray-400'}`}>{title}</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavButton;
