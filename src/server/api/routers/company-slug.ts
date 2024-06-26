1847a9753f9715f88a71601da16c71b75b8beba0
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-06-26 18:04:21 +0700

    individual company slug

---
+++ src/server/api/routers/company-slug.ts
@@ -0,0 +1,83 @@
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+import { z } from "zod";
+
+export const companySlugRouter = createTRPCRouter({
+  getSpecificCompany: protectedProcedure
+    .input(
+      z.object({
+        companyId: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      try {
+        const company = await ctx.db.company.findUnique({
+          where: {
+            id: input.companyId,
+          },
+          include: {
+            sectors: true,
+          },
+        });
+        return company;
+      } catch (e) {
+        console.log(e);
+      }
+    }),
+    getCompanyPrimarySector: protectedProcedure
+    .input(
+      z.object({
+        companyId: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      try {
+        const companyWithPrimarySector = await ctx.db.company.findUnique({
+          where: {
+            id: input.companyId,
+          },
+          include: {
+            sectors: {
+              where: {
+                primary: true,
+              },
+            },
+          },
+        });
+        const primarySector = companyWithPrimarySector?.sectors[0]; 
+        return primarySector;
+      } catch (e) {
+        console.log(e);
+        throw new Error('Failed to fetch the primary sector');
+      }
+    }),
+    getCompanyOtherSector: protectedProcedure
+      .input(
+        z.object({
+          companyId: z.string()
+        })
+      )
+      .query(async ({ctx, input}) => {
+        try {
+          const company = await ctx.db.company.findUnique({
+            where: {
+              id: input.companyId
+            },
+            select: {
+              sectors: {
+                where: {
+                  primary: false
+                }
+              }
+            }
+          })
+          return company
+        }catch(e) {
+          console.log(e)
+        }
+      })
+});
