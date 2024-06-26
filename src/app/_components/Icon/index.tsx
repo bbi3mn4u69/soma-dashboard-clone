5d734d5a15883871bc13ef4c2e6f1b067ed03bbc
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-06-26 18:00:45 +0700

    add icon

--- src/app/_components/Icon/index.tsx
+++ src/app/_components/Icon/index.tsx
@@ -219,3 +219,22 @@ export const News = () => {
     </svg>
   );
 };
+
+export const Arrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="#4f46e5"
+      className="size-5"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+      />
+    </svg>
+  );
+};
