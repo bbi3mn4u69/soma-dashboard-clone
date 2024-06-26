ebeee015f094e83e21244acffb476aa141d6c108
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-06-26 18:01:16 +0700

    filter function for portfolio

--- src/app/_components/Portfolio/Company.tsx
+++ src/app/_components/Portfolio/Company.tsx
@@ -12,10 +12,21 @@ import Image from "next/image";
 
 import { getRandomDateAsString } from "../shared/RandomDate";
 import { api } from "~/trpc/react";
+import { useAppContext } from "~/app/_components/context";
+import { Skeleton } from "@nextui-org/react";
 
 const Company = () => {
-  const { data } = api.dashboardPortfolio.getPortfolio.useQuery();
-  const { data: totalCompanies } = api.dashboardPortfolio.getTotalCompaniesNum.useQuery();
+  const {
+    portforlioIndustrySelected,
+    portforlioValuationSelected,
+    portforlioRegionSelected,
+  } = useAppContext();
+  const { data, isLoading } = api.dashboardPortfolio.portfolioFilter.useQuery({
+    value: portforlioValuationSelected,
+    industry: portforlioIndustrySelected,
+    region: portforlioRegionSelected,
+  });
+  
 
   return (
     <>
@@ -25,9 +36,13 @@ const Company = () => {
             <TableColumn>
               <div className="flex flex-row items-center gap-1">
                 <div>Company</div>
-                <div className="rounded-full bg-gray-300 px-2 py-1 text-xs text-gray-800">
-                  {totalCompanies}
-                </div>
+                {isLoading ? (
+                  <Skeleton className="h-6 w-10 rounded-full " />
+                ) : (
+                  <div className="rounded-full bg-gray-300 px-2 py-1 text-xs text-gray-800">
+                    {data?.length}
+                  </div>
+                )}
               </div>
             </TableColumn>
             <TableColumn>
@@ -38,53 +53,83 @@ const Company = () => {
             <TableColumn>Valuation</TableColumn>
           </TableHeader>
           <TableBody>
-            {(data ?? []).map((company) => (
-              <TableRow key={company.id}>
-                <TableCell>
-                  <div className="flex flex-row items-center gap-2">
-                    <Image
-                      src={company.logoUrl ?? ""}
-                      alt="Company Logo"
-                      width={45}
-                      height={45}
-                      className="rounded-md"
-                    />
-                    <div className="flex flex-col gap-0">
-                      <div className="text-sm font-medium">{company.name}</div>
-                      <div className="text-xs font-medium text-gray-400">
-                        {company.oneLiner}
+            {isLoading
+              ? [...Array(10)].map((_, index) => (
+                  <TableRow key={index}>
+                    <TableCell>
+                      <div className="flex flex-row items-center gap-2">
+                        <Skeleton className="h-[45px] w-[45px] rounded-md " />
+                        <div className="flex flex-col gap-1">
+                          <Skeleton className="h-4 w-10 rounded " />
+                          <Skeleton className="h-4 w-96 rounded " />
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                </TableCell>
-                <TableCell className="font-medium">{company.region}</TableCell>
-                <TableCell>
-                  <Chip
-                    classNames={{
-                      base: " bg-green-200 p-0 rounded-full",
-                      content: " text-green-800 text-xs font-semibold",
-                    }}
-                    radius="sm"
-                  >
-                    {company.sectors[0]?.name ?? ""}
-                  </Chip>
-                </TableCell>
-                <TableCell className="font-medium text-gray-400">
-                  {getRandomDateAsString(2010, 2024, "MM/DD/YYYY")}
-                </TableCell>
-                <TableCell>
-                  <Chip
-                    classNames={{
-                      base: " bg-green-200 p-0 rounded-full",
-                      content: " text-green-800 text-xs font-semibold",
-                    }}
-                    radius="sm"
-                  >
-                    {company.valuation}
-                  </Chip>
-                </TableCell>
-              </TableRow>
-            ))}
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton className="h-5 w-10 rounded " />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton className="h-5 w-10 rounded-full bg-green-300  " />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton className="h-5 w-24 rounded " />
+                    </TableCell>
+                    <TableCell>
+                      <Skeleton className="h-5 w-10 rounded-full bg-green-300 " />
+                    </TableCell>
+                  </TableRow>
+                ))
+              : (data ?? []).map((company) => (
+                  <TableRow key={company.id}>
+                    <TableCell>
+                      <div className="flex flex-row items-center gap-2">
+                        <Image
+                          src={company.logoUrl ?? ""}
+                          alt="Company Logo"
+                          width={45}
+                          height={45}
+                          className="rounded-md"
+                        />
+                        <div className="flex flex-col gap-0">
+                          <div className="text-sm font-medium">
+                            {company.name}
+                          </div>
+                          <div className="text-xs font-medium text-gray-400">
+                            {company.oneLiner}
+                          </div>
+                        </div>
+                      </div>
+                    </TableCell>
+                    <TableCell className="font-medium">
+                      {company.region}
+                    </TableCell>
+                    <TableCell>
+                      <Chip
+                        classNames={{
+                          base: " bg-green-200 p-0 rounded-full",
+                          content: " text-green-800 text-xs font-semibold",
+                        }}
+                        radius="sm"
+                      >
+                        {company.sectors[0]?.name ?? ""}
+                      </Chip>
+                    </TableCell>
+                    <TableCell className="font-medium text-gray-400">
+                      {getRandomDateAsString(2010, 2024, "MM/DD/YYYY")}
+                    </TableCell>
+                    <TableCell>
+                      <Chip
+                        classNames={{
+                          base: " bg-green-200 p-0 rounded-full",
+                          content: " text-green-800 text-xs font-semibold",
+                        }}
+                        radius="sm"
+                      >
+                        {company.valuation}
+                      </Chip>
+                    </TableCell>
+                  </TableRow>
+                ))}
           </TableBody>
         </Table>
       </div>
--- src/app/_components/Portfolio/Filter.tsx
+++ src/app/_components/Portfolio/Filter.tsx
@@ -1,46 +1,129 @@
 import { RadioGroup, Radio } from "@nextui-org/react";
-
-const Filter = () => {
+import { useAppContext } from "../context";
+import { useEffect } from "react";
+const Valuation = () => {
   return (
-    <div className="flex flex-grow flex-col flex-nowrap space-y-3 text-nowrap px-7">
+    <>
+      <div className="flex flex-row items-center gap-1">
+        <div>Valuation</div>
+        <div className="h-3 w-3 rounded-sm bg-purple-300"></div>
+      </div>
+    </>
+  );
+};
+const Industry = () => {
+  return (
+    <>
+      <div className="flex flex-row items-center gap-1">
+        <div>Industry</div>
+        <div className="h-3 w-3 rounded-sm bg-green-300"></div>
+      </div>
+    </>
+  );
+};
+const Region = () => {
+  return (
+    <>
+      <div className="flex flex-row items-center gap-1">
+        <div>Region</div>
+        <div className="h-3 w-3 rounded-sm bg-indigo-300"></div>
+      </div>
+    </>
+  );
+};
+const Filter = () => {
+  const {
+    setPortforlioValuationSelected,
+    setPortforlioIndustrySelected,
+    setPortforlioRegionSelected,
+  } = useAppContext();
 
-      <RadioGroup
-        label="Valuations"
-        defaultValue="All"
-        color="primary"
-        size="sm"
-        classNames={{
-          label: "text-black",
-        }}
-      >
-        <Radio value="All" className="text-sm">
-          All
-        </Radio>
-        <Radio value="+5b">+5b</Radio>
-        <Radio value="1-5b">1-5b</Radio>
-        <Radio value="500m-1b">500m-1b</Radio>
-        <Radio value="100-500m">100-500m</Radio>
-        <Radio value="50-100m">50-100m</Radio>
-        <Radio value="<50m">{"<50m"}</Radio>
-        <Radio value="N/A">N/A</Radio>
-      </RadioGroup>
-      
-      <RadioGroup
-        label="Industry"
-        defaultValue="All"
-        color="primary"
-        size="sm"
-        classNames={{
-          label: "text-black",
-        }}
-      >
-        <Radio value="All" className="text-sm">
-          All
-        </Radio>
-        <Radio value="B2B/SaaS">B2B/SaaS</Radio>
-        <Radio value="AI">AI</Radio>
-        <Radio value="FinTech">FinTech</Radio>
-      </RadioGroup>
+  return (
+    <div className="flex h-full flex-grow flex-col flex-nowrap space-y-3 overflow-y-scroll text-nowrap px-7 pb-7">
+      <div className="flex flex-col justify-start gap-2">
+        <div className="text-start">
+          <RadioGroup
+            defaultValue="All"
+            color="primary"
+            size="sm"
+            classNames={{
+              label: "text-black",
+            }}
+            onValueChange={setPortforlioValuationSelected}
+          >
+            <Valuation></Valuation>
+            <Radio value="All" className="text-sm">
+              All
+            </Radio>
+            <Radio value="+5b">+5b</Radio>
+            <Radio value="1-5b">1-5b</Radio>
+            <Radio value="500m-1b">500m-1b</Radio>
+            <Radio value="100-500m">100-500m</Radio>
+            <Radio value="50-100m">50-100m</Radio>
+            <Radio value="<50m">{"<50m"}</Radio>
+            <Radio value="N/A">N/A</Radio>
+          </RadioGroup>
+        </div>
+
+        <div className="text-start">
+          <RadioGroup
+            defaultValue="All"
+            color="primary"
+            size="sm"
+            classNames={{
+              label: "text-black",
+            }}
+            onValueChange={setPortforlioIndustrySelected}
+          >
+            <Industry></Industry>
+            <Radio value="All" className="text-sm">
+              All
+            </Radio>
+            <Radio value="B2B/SaaS">B2B/SaaS</Radio>
+            <Radio value="AI">AI</Radio>
+            <Radio value="FinTech">FinTech</Radio>
+            <Radio value="Logistics">Logistics</Radio>
+            <Radio value="CleanTech">CleanTech</Radio>
+            <Radio value="FrontierTech">FrontierTech</Radio>
+            <Radio value="PropTech">PropTech</Radio>
+            <Radio value="HealthTech">HealthTech</Radio>
+            <Radio value="Crypto">Crypto</Radio>
+            <Radio value="Consumer">Consumer</Radio>
+          </RadioGroup>
+        </div>
+        <div className="text-start">
+          <RadioGroup
+            defaultValue="All"
+            color="primary"
+            size="sm"
+            classNames={{
+              label: "text-black",
+            }}
+            className="text-start"
+            onValueChange={setPortforlioRegionSelected}
+          >
+            <Region></Region>
+            <Radio value="All" className="text-sm">
+              All
+            </Radio>
+            <Radio value="US">US</Radio>
+            <Radio value="SF">SF</Radio>
+            <Radio value="NYC">NYC</Radio>
+            <Radio value="LA">LA</Radio>
+            <Radio value="Miami">Miami</Radio>
+            <Radio value="Boston">Boston</Radio>
+            <Radio value="Austin">Austin</Radio>
+            <Radio value="Europe">Europe</Radio>
+            <Radio value="MENA">MENA</Radio>
+            <Radio value="SEA">SEA</Radio>
+            <Radio value="India">India</Radio>
+            <Radio value="UK">UK</Radio>
+            <Radio value="Brazil">Brazil</Radio>
+            <Radio value="Mexico">Mexico</Radio>
+            <Radio value="LatAm">LatAm</Radio>
+          </RadioGroup>
+        </div>
+      </div>
     </div>
   );
 };
