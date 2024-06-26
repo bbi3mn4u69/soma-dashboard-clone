9ed64db8bafcdb9dde096334269cd983ba1cbb1a
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-06-26 18:00:55 +0700

    add  chips

--- src/app/_components/Portfolio/Chips.tsx
+++ src/app/_components/Portfolio/Chips.tsx
@@ -1,6 +1,7 @@
 import { Chip } from "@nextui-org/react";
+import { useAppContext } from "../context";
 
-const SomaFilterChip = () => {
+const Chip1 = ({content}:{content:string}) => {
   return (
     <>
       <div>
@@ -11,18 +12,54 @@ const SomaFilterChip = () => {
             content: "font-semibold text-purple-600",
           }}
         >
-          All
+          {content}
         </Chip>
       </div>
     </>
   );
 };
-
-const TopChips = () => {
+const Chip2 = ({content}:{content:string}) => {
   return (
     <>
       <div>
-        <SomaFilterChip />
+        <Chip
+          size="sm"
+          classNames={{
+            base: "bg-green-200",
+            content: "font-semibold text-green-600",
+          }}
+        >
+          {content}
+        </Chip>
+      </div>
+    </>
+  );
+};
+const Chip3 = ({content}:{content:string}) => {
+  return (
+    <>
+      <div>
+        <Chip
+          size="sm"
+          classNames={{
+            base: "bg-indigo-200",
+            content: "font-semibold text-indigo-600",
+          }}
+        >
+          {content}
+        </Chip>
+      </div>
+    </>
+  );
+};
+const TopChips = () => {
+  const { portforlioIndustrySelected, portforlioValuationSelected, portforlioRegionSelected } = useAppContext();
+  return (
+    <>
+      <div className="flex flex-row items-center gap-2">
+        <Chip1 content={portforlioValuationSelected}/>
+        <Chip2 content={portforlioIndustrySelected}/>
+        <Chip3 content={portforlioRegionSelected}/>
       </div>
     </>
   );
