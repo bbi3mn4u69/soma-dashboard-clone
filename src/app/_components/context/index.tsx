2e6e5da84d158c3711b7f22302e45283a0e0bf60
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-06-26 18:00:19 +0700

    add context

--- src/app/_components/context/index.tsx
+++ src/app/_components/context/index.tsx
@@ -5,18 +5,39 @@ interface AppContextProps {
   setIndustriesSelected: React.Dispatch<React.SetStateAction<string>>;
   regionsSelected: string;
   setRegionsSelected: React.Dispatch<React.SetStateAction<string>>;
+  // portforlio page
+  portforlioValuationSelected: string;
+  setPortforlioValuationSelected: React.Dispatch<React.SetStateAction<string>>;
+  portforlioIndustrySelected: string;
+  setPortforlioIndustrySelected: React.Dispatch<React.SetStateAction<string>>;
+  portforlioRegionSelected: string;
+  setPortforlioRegionSelected: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export const AppContext = createContext<AppContextProps | undefined>(undefined);
 const AppContextProvider = ({ children }: { children: ReactNode }) => {
   const [industriesSelected, setIndustriesSelected] = useState<string>("All");
   const [regionsSelected, setRegionsSelected] = useState<string>("All");
+  const [portforlioValuationSelected, setPortforlioValuationSelected] =
+    useState<string>("All");
+  const [portforlioIndustrySelected, setPortforlioIndustrySelected] =
+    useState<string>("All");
+  const [portforlioRegionSelected, setPortforlioRegionSelected] =
+    useState<string>("All");
+
   const value = {
     industriesSelected,
     setIndustriesSelected,
     regionsSelected,
     setRegionsSelected,
+    portforlioValuationSelected,
+    setPortforlioValuationSelected,
+    portforlioIndustrySelected,
+    setPortforlioIndustrySelected,
+    portforlioRegionSelected,
+    setPortforlioRegionSelected,
   };
+
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 export default AppContextProvider;
