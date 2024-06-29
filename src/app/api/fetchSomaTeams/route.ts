e1815cf45844e4af30fb2a007086bb509c041046
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-06-29 07:59:37 +0700

    fix

--- src/app/api/fetchSomaTeams/route.ts
+++ src/app/api/fetchSomaTeams/route.ts
@@ -19,7 +19,7 @@ interface TeamData {
 export async function GET() {
   try {
     const url =
-      "https://somacap.com/_next/data/ZrmRePndS-s_oHjKPd-Zx/team.json";
+      "https://somacap.com/_next/data/WIWOTQ4_9C7vGSg3nrzal/team.json";
     const data = (await fetchDataFromURL(url)) as TeamData;
     const teams: Team[] = data.pageProps.team;
     console.log("running here");
