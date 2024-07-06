bfe61aa9593d4fded68d545a5474ebe7f9053a5e
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-07-06 15:57:13 +0700

    add admin page

--- src/app/_components/shared/SidePanel.tsx
+++ src/app/_components/shared/SidePanel.tsx
@@ -7,27 +7,29 @@ import { useRouter } from "next/navigation";
 import { usePathname } from "next/navigation";
 import { useAppContext } from "../context";
 const SidePanel = () => {
-  
   const { isMobileSideBarOpen } = useAppContext();
   const router = useRouter();
   const logoClick = () => {
     router.push("/");
-  }
+  };
   const [activeButton, setActiveButton] = useState<string>(() => {
-    if (typeof window !== 'undefined') {
-      return localStorage.getItem('activeButton') ?? "Home";
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("activeButton") ?? "Home";
     }
     return "Home";
   });
   const pathname = usePathname();
-   // Update local storage when activeButton changes
-   useEffect(() => {
-    localStorage.setItem('activeButton', activeButton);
+  // Update local storage when activeButton changes
+  useEffect(() => {
+    localStorage.setItem("activeButton", activeButton);
   }, [activeButton]);
   return (
     <>
-      <div className='min-h-screen bg-purple-900'>
-        <div className="flex items-center justify-center py-4 cursor-pointer " onClick={logoClick}>
+      <div className="min-h-screen bg-purple-900">
+        <div
+          className="flex cursor-pointer items-center justify-center py-4 "
+          onClick={logoClick}
+        >
           <GrayLogo />
         </div>
         <div className="mx-1 flex flex-col items-center justify-center">
@@ -69,7 +71,9 @@ const SidePanel = () => {
           <NavButton
             title="Admin"
             icon={<Admin active={activeButton === "Admin"} />}
-            onClick={() => setActiveButton("Admin")}
+            onClick={() => {
+              setActiveButton("Admin"), router.push("/dashboard/admin");
+            }}
             isActive={activeButton === "Admin"}
           />
           <NavButton
