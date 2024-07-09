dc533fbcbdbce4fcd2a7dfbb0284e969923d4c0e
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-07-09 11:48:59 +0700

    package update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,48 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
-const {nextui} = require("@nextui-org/react");
+import type { Config } from "tailwindcss"
+import {fontFamily} from "tailwindcss/defaultTheme"
+const {nextui} = require("@nextui-org/react")
 
-export default {
+const config = {
+  darkMode: ["class"],
   content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
     "./src/**/*.tsx",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  darkMode: "class",
-  plugins: [nextui()],
-} satisfies Config;
+  plugins: [require("tailwindcss-animate"), nextui()],
+} satisfies Config
+
+export default config
