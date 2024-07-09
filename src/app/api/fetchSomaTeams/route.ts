68c9895ed3222b74b6a0f2beb2d8f51309e5869e
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-07-09 12:09:31 +0700

    error fix

--- src/app/api/fetchSomaTeams/route.ts
+++ src/app/api/fetchSomaTeams/route.ts
@@ -1,57 +1,57 @@
-import { NextRequest, NextResponse } from "next/server";
-import { db } from "~/server/db";
-import { fetchDataFromURL } from "../fetchSomaCompanies/routeHelper";
+// import { NextRequest, NextResponse } from "next/server";
+// import { db } from "~/server/db";
+// import { fetchDataFromURL } from "../fetchSomaCompanies/routeHelper";
 
-interface Team {
-  name: string;
-  role: string;
-  imageUrl: string;
-  path: string;
-  bio: string[];
-}
+// interface Team {
+//   name: string;
+//   role: string;
+//   imageUrl: string;
+//   path: string;
+//   bio: string[];
+// }
 
-interface TeamData {
-  pageProps: {
-    team: Team[];
-  };
-}
+// interface TeamData {
+//   pageProps: {
+//     team: Team[];
+//   };
+// }
 
-export async function GET() {
-  try {
-    const url =
-      "https://somacap.com/_next/data/Vn7MBsrRy0OYJ8U0hfe63/team.json";
-    const data = (await fetchDataFromURL(url)) as TeamData;
-    const teams: Team[] = data.pageProps.team;
-    console.log("running here");
-    await saveTeamsToDB(teams);
-    return NextResponse.json(data.pageProps.team);
-  } catch (e) {
-    throw new Error(e as string);
-  }
+// export async function GET() {
+//   try {
+//     const url =
+//       "https://somacap.com/_next/data/Vn7MBsrRy0OYJ8U0hfe63/team.json";
+//     const data = (await fetchDataFromURL(url)) as TeamData;
+//     const teams: Team[] = data.pageProps.team;
+//     console.log("running here");
+//     await saveTeamsToDB(teams);
+//     return NextResponse.json(data.pageProps.team);
+//   } catch (e) {
+//     throw new Error(e as string);
+//   }
   
-}
+// }
 
-async function saveTeamsToDB(teams: Team[]) {
-  for (const person of teams) {
-    try {
-      // Check if the team already exists in the database
-      const existingTeam = await db.teams.findUnique({
-        where: { slug: person.path },
-      });
-      if (!existingTeam) {
-        await db.teams.create({
-          data: {
-            name: person.name,
-            role: person.role,
-            imageUrl: person.imageUrl,
-            slug: person.path,
-            bio: person.bio.join(" "),
-          },
-        });
-      }
-    } catch (e) {
-      console.error("Failed to save team:", e);
-      throw e;
-    }
-  }
-}
+// async function saveTeamsToDB(teams: Team[]) {
+//   for (const person of teams) {
+//     try {
+//       // Check if the team already exists in the database
+//       const existingTeam = await db.teams.findUnique({
+//         where: { slug: person.path },
+//       });
+//       if (!existingTeam) {
+//         await db.teams.create({
+//           data: {
+//             name: person.name,
+//             role: person.role,
+//             imageUrl: person.imageUrl,
+//             slug: person.path,
+//             bio: person.bio.join(" "),
+//           },
+//         });
+//       }
+//     } catch (e) {
+//       console.error("Failed to save team:", e);
+//       throw e;
+//     }
+//   }
+// }
