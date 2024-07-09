d0b5b26a1818b8996c170e2ffb6881b4b386f266
Quang Huy Pham <106767595+bbi3mn4u69@users.noreply.github.com>
2024-07-09 14:57:57 +0700

    eslint error fix

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,37 +1,42 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
 import { NextResponse } from 'next/server';
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 
-const url: string = 'https://www.science.org/content/article/dreamlike-pig-hunting-scene-world-s-oldest-figurative-art';
+// puppeteer.use(StealthPlugin())
+
+// const url: string = 'https://techcrunch.com/2024/07/05/epic-games-calls-out-apple-for-rejecting-its-games-store-in-the-eu/';
 
 
-export async function GET(req: Request, res: Response) {
-  const retries: number = 5;
-  const backoff: number = 5000;
+// export async function GET(req: Request, res: Response) {
+//   try {
+//     const browser = await puppeteer.launch({ headless: true, defaultViewport: null  });
+//     const page = await browser.newPage();
+//     // Set user agent
+//     await page.setUserAgent(
+//       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+//     );
 
-  try {
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
+//     await page.goto(url, { waitUntil: 'domcontentloaded' });
 
-    // Set user agent
-    await page.setUserAgent(
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-    );
+//     const links = await page.evaluate(() => {
+//       const anchorElements = Array.from(document.querySelectorAll('body a'));
+//       return anchorElements.some(a => (a as HTMLAnchorElement).href.startsWith('https://www.rippling.com'));
+//     });
 
-    await page.goto(url, { waitUntil: 'domcontentloaded' });
+//     await browser.close();
+//     return NextResponse.json({ links });
+//   } catch (error) {
+//     console.log(`Error for URL ${url}: ${error}`);
+//   }
+//   return NextResponse.json({ error: 'Failed to fetch links' });
+// }
 
-    const links = await page.evaluate(() => {
-      const anchorElements = Array.from(document.querySelectorAll('body a'));
-      return anchorElements
-        .map(a => (a as HTMLAnchorElement).href)
-        .filter(href => href.startsWith('https://'));
-    });
 
-    await browser.close();
-    return NextResponse.json(links);
-  } catch (error) {
-    console.log(`Error for URL ${url}: ${error}`);
-  }
+
+
+
+export async function GET () {
   return NextResponse.json({ error: 'Failed to fetch links' });
 }
